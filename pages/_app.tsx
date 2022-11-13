67dea09c989bbb9babfdd057d9c5ae7d1d04cb09
Bruno <brunonoriller@gmail.com>
2022-11-13 11:24:46 -0300

    base setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
+import { SideBar } from '../src/SideBar';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>React 102</title>
+        <meta
+          name='description'
+          content='Basics of React after the 101 stuff (not included).'
+        />
+      </Head>
+      <header className='h-auto mb-2'>
+        <h1 className='font-bold text-3xl border-b-4'>React 102</h1>
+        <span className='text-sm'>
+          Basics of React after the 101 stuff (not included).
+        </span>
+      </header>
+      <div className='mb-auto h-auto flex'>
+        <aside className='w-1/6 h-full px-2'>
+          <SideBar />
+        </aside>
+        <main className='w-5/6 border-2'>
+          <Component {...pageProps} />
+        </main>
+      </div>
+      <footer className='h-auto border-t-2'>
+        {`Bruno Noriller @${new Date().getFullYear()}`}
+      </footer>
+    </>
+  );
 }
---
+++ src/SideBar.tsx
@@ -0,0 +1,11 @@
+export function SideBar() {
+  return (
+    <ol>
+      <li>1 links</li>
+      <li>2 links</li>
+      <li>3 links</li>
+      <li>4 links</li>
+      <li>5 links</li>
+    </ol>
+  );
+}
