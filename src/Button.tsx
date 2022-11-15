c08b9c3be4efa53062be7fde97a66fbfd677e25f
Bruno <brunonoriller@gmail.com>
2022-11-15 15:55:28 -0300

    update button component

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 type Props = {
   children: React.ReactNode;
   onClick: (event: React.MouseEvent<HTMLElement>) => void;
+  className?: string;
 };
 
-export function Button({ children, onClick }: Props) {
+export function Button({ children, onClick, className = '' }: Props) {
   return (
-    <div className='my-4 border-2 rounded-lg p-2 w-max hover:text-blue-500'>
+    <div
+      className={`my-4 border-2 rounded-lg p-2 w-max hover:text-blue-500 ${className}`}>
       <button onClick={onClick}>{children}</button>
     </div>
   );
