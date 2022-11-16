2b09db7d01e36623ee410c768ee46e485267e5b8
Bruno <brunonoriller@gmail.com>
2022-11-15 21:37:04 -0300

    update link to repo

--- src/RepoLink.tsx
+++ src/RepoLink.tsx
@@ -2,7 +2,7 @@ export function RepoLink() {
   return (
     <a
       target='_blank'
-      href='https://github.com/noriller'
+      href='https://github.com/noriller/react102'
       rel='noreferrer'
       className='underline hover:text-blue-500 decoration-cyan-200 decoration-2'>
       Github Repo for This Project
